4aef4a5b4406f7907faf41a5085d797c05257eab
jayceeasparagus <118419528+jayceeasparagus@users.noreply.github.com>
2025-12-24 22:06:57 -0800

    Added file reading

--- src/main.c
+++ src/main.c
@@ -3,21 +3,43 @@
 #include "ast.h"
 #include "eval.h"
 #include "env.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    const char *source =
-    "module add(input a, input b, output c) {\n"
-    "c = a + b;\n"
-    "}\n"
-    "num x = 2;\n"
-    "num y = 3;\n"
-    "num z = 0;\n"
-    "add(x, y, z);\n"
-    "print(z);\n"
-    "bool h = true;\n"
-    "print(!h || true);\n";
+char *read_file(const char *filename) {
+    FILE *file = fopen(filename, "rb");
 
-    Lexer lexer = { source, 0, 1 };
+    if (!file) {
+        printf("ERROR: Could not open file '%s'", filename);
+        exit(1);
+    }
+
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    rewind(file);
+
+    char *buffer = malloc(size + 1);
+    if (!buffer) {
+        printf("ERROR: Out of memory\n");
+        exit(1);
+    }
+
+    fread(buffer, 1, size, file);
+    buffer[size] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("ERROR: Format is 'jh <filename.jh>'\n");
+        return 1;
+    }
+
+    char *source = read_file(argv[1]);
+
+    Lexer lexer = {.src = source, .pos = 0, .line = 1};
 
     Parser parser;
     parser_init(&parser, &lexer);
@@ -26,6 +48,8 @@ int main() {
 
     env_init();
     eval(program);
+
+    free(source);
 
     return 0;
 }
